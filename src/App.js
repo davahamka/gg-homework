a8246252386c659f4b933a3377927419b15055e5
davahamka <davahamka512@gmail.com>
2022-03-19 14:59:57 +0700

    Add spotify key

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState("");
+  const [dataUser, setDataUser] = useState(null);
+  const [status, setStatus] = useState("idle") 
+
+  useEffect(()=>{
+    setStatus("pending")
+    fetch('https://accounts.spotify.com/api/token', {
+      method:'POST',
+      headers:{
+        'Authorization': 'Basic ' +  btoa(process.env.REACT_APP_SPOTIFY_CLIENT_ID + ':' + process.env.REACT_APP_SPOTIFY_CLIENT_SECRET),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({grant_type: 'client_credentials'})
+    }).then(res=>res.json())
+      .then(data=>setToken(data.access_token))
+  },[])
+
+  useEffect(()=>{
+    if(token) {
+      fetch("https://api.spotify.com/v1/me", {
+      method:'GET',
+      headers:{
+        "Authorization": `Bearer BQBrctexL2bEbzb-qwWcHw08jUObSGc7T5PggxhL6pp2dIA73DEwWri6-DUCkhTURex314MHdtCbdfHmhxL7k-qbq9Qib-P_VwVSFhxUmOVlqVPoTcFgXsuvsDJhUHHpavnwQaQ7aHqhjyayvk6ZeWuGCg`
+      }
+    }).then((res)=>res.json())
+      .then((data)=>{setDataUser(data);setStatus("success")})
+      .catch(err=>{
+        console.log(err)
+      }
+    )
+    }
+  },[token])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       {status==="success" ?<img src={dataUser.images[0].url} className="App-logo" alt="logo" /> :status === "pending" || status === "idle" ? <img src={logo} className="App-logo" alt="logo" /> : ""}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p className='text-display'>{dataUser?.display_name ?? "display_name"} - {dataUser?.country ?? "country"}</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}</p>
       </header>
     </div>
   );
