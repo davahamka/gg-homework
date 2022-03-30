5bc173630894ca9a928230ebf53712283482b9ef
davahamka <davahamka512@gmail.com>
2022-03-30 10:10:31 +0700

    Finish module 3 session 1

---
+++ src/utils/formatToMinutesSecond.js
@@ -0,0 +1,5 @@
+export const formatToMinutesSecond = (timeMs) => {
+  const minutes = Math.floor(timeMs / 60000);
+  const seconds = ((timeMs % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+};
